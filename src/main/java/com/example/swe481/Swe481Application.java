d0032f5a4c07f7a76aaedccef2d990fa1d4bb3bb
Mohammed <444102192@student.ksu.edu.sa>
2025-09-18 11:13:54 +0300

    Initial commit

---
+++ src/main/java/com/example/swe481/Swe481Application.java
@@ -0,0 +1,13 @@
+package com.example.swe481;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe481Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swe481Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swe481/Swe481ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swe481;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe481ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
